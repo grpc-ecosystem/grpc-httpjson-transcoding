54d9d9664b8258e0c1ee36b2bc94116c18f5725d
Hongru Xiang <hongrux@google.com>
2022-09-20 00:58:38 +0000

    Add container push to CI

--- script/ci.sh
+++ script/ci.sh
@@ -18,3 +18,5 @@
 
 bazel build //...
 bazel test //... --test_output=errors
+
+bazel run //perf_benchmark:benchmark_main_image_push --define=PUSH_PROJECT=cloudesf-testing --define=PUSH_TAG=github-latest
